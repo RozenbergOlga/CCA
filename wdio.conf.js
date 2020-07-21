21a26f53519668a3aa3dc5dece15884b812a6384
OlgaRozenberg <froggi3377@gmail.com>
2020-07-20 19:11:01 -0700

    change wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -6,7 +6,9 @@ exports.config = {
     //
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
+
     runner: 'local',
+
     //
     // ==================
     // Specify Test Files
@@ -16,10 +18,12 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
+
     specs: [
         './test/*.js'
     ],
     // Patterns to exclude.
+
     exclude: [
 
         // 'path/to/excluded/files'
