c41d84e7f0de8b192d743cef5ee5070a22430d8b
OlgaRozenberg <froggi3377@gmail.com>
2020-08-03 23:30:59 -0700

    change elements.js

--- test/elements.js
+++ test/elements.js
@@ -62,39 +62,32 @@ describe('Complex Counter App', function () {
             const actual = $(selectorGen.totalResult).getText();
             expect(actual).toEqual(expectedGen.totalResult);
         })
-
         it('TC-011 Label for Add Name Field = Enter Counter Title:', function () {
             const actual = $$(selectorGen.addNameFieldLabel)[$$(selectorGen.addNameFieldLabel).length-2].getText();
             expect(actual).toEqual(expectedGen.addNameFieldLabel);
         })
-
         it('TC-012 Placeholder for Add Name Field = Counter Name', function () {
             const actual = $(selectorGen.addNameField).getValue();
             expect(actual).toEqual(expectedGen.addNameField);
         })
-
         it('TC-013 Label for Default Value Field = Enter Initial Count:', function () {
             const actual = $$(selectorGen.defaultValueFieldLabel)[$$(selectorGen.defaultValueFieldLabel).length-1].getText();
             expect(actual).toEqual(expectedGen.defaultValueFieldLabel);
         })
-
         it('TC-014 Placeholder for Default Value Field = 50', function () {
             const actual = $(selectorGen.defaultValueField).getValue();
             expect(actual).toEqual(expectedGen.defaultValueField);
         })
-
         it('TC-015 Add Counter = ADD COUNTER', function () {
             const actual = $(selectorGen.addCounterBtn).getText();
             expect(actual).toEqual(expectedGen.addCounterBtn);
         })
-
     });
     describe('Default Counter Elements exist', function () {
         it('TC-016 Counter Name', function () {
             const actual = $$(selectorCnt.counterName)[1].isDisplayed();
             expect(actual).toEqual(true);
         })
-
         it('TC-017 Count Value', function () {
             const actual = $(selectorCnt.countValue).isDisplayed();
             expect(actual).toEqual(true);
