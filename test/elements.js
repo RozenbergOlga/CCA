0f03eb70cfc467242af604241055e58b0046e224
OlgaRozenberg <froggi3377@gmail.com>
2020-08-06 21:07:29 -0700

    change elements.js

--- test/elements.js
+++ test/elements.js
@@ -13,6 +13,7 @@ describe('Complex Counter App', function () {
             expect(title).toEqual('Complex Counter App');
         })
     });
+
     describe('Elements exist', function () {
         it('TC-002 Header', function () {
             const actual = $(selectorGen.header).isDisplayed();
@@ -62,10 +63,12 @@ describe('Complex Counter App', function () {
             const actual = $(selectorGen.totalResult).getText();
             expect(actual).toEqual(expectedGen.totalResult);
         })
+
         it('TC-011 Label for Add Name Field = Enter Counter Title:', function () {
             const actual = $$(selectorGen.addNameFieldLabel)[$$(selectorGen.addNameFieldLabel).length-2].getText();
             expect(actual).toEqual(expectedGen.addNameFieldLabel);
         })
+
         it('TC-012 Placeholder for Add Name Field = Counter Name', function () {
             const actual = $(selectorGen.addNameField).getValue();
             expect(actual).toEqual(expectedGen.addNameField);
