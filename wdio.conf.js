1d90d15b838de33e9b8e47d483c118db0f80f0fe
OlgaRozenberg <froggi3377@gmail.com>
2020-08-06 21:11:13 -0700

    add wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,21 +19,13 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-
     specs: [
-
-
         './test/*.js'
-
     ],
-
     // Patterns to exclude.
-
     exclude: [
-
         // 'path/to/excluded/files'
     ],
-
     //
     // ============
     // Capabilities
@@ -50,9 +42,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-
     maxInstances: 1,
-
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
