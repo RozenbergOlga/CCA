49463da99c5de19a8fd70ea10a5e3dfd7b49420d
OlgaRozenberg <froggi3377@gmail.com>
2020-09-17 23:43:20 -0700

    change defaultFunctionality.js

--- test/defaultFunctionality.js
+++ test/defaultFunctionality.js
@@ -3,7 +3,6 @@ describe('Default counter functionality', function () {
     describe('Calculation works', function () {
 
         it('TC-021 Subtract 1 gives -1', function () {
-
             browser.url('https://likejean.github.io/homework-5/');
             browser.pause(2000);
             const button = $$('.btn-black')[0];
@@ -11,7 +10,6 @@ describe('Default counter functionality', function () {
             const countValue = $('.badge').getText();
             expect(countValue).toEqual('-1');
         })
-
         it('TC-022 Add 3 gives 2', function () {
 
             browser.pause(2000);
@@ -20,6 +18,5 @@ describe('Default counter functionality', function () {
             const countValue = $('.badge').getText();
             expect(countValue).toEqual('2');
         })
-
     });
 });
