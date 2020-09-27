5da6c36c5850bea8a0471b0c9b25d2315dffa98e
OlgaRozenberg <froggi3377@gmail.com>
2020-09-26 22:34:43 -0700

    add babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,21 @@
 module.exports ={
 
     "presets": [
+
         ['@babel/preset-env',
+
             {
+
                 "targets":
+
                     {
+
                         "node": 12
+
                     }
+
             }]
+
     ]
 
 }
